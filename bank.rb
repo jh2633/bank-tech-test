d6e61a54e3aebc152ac816a43c6691a0dbc2f757
Joseph <joseph.jr@gmail.com>
2016-10-17 17:49:35 +0100

    create bank class and add rspec gem

---
+++ bank.rb
@@ -0,0 +1,29 @@
+class Bank
+
+def initialize
+  @balance = 0
+  @book = []
+end
+
+def deposit
+
+end
+
+def withdraw
+
+end
+
+
+def print
+puts "date || credit || debit || balance"
+
+end
+
+end
+
+
+
+
+14/01/2012 ||  500.00 || || 2500.00
+13/01/2012 || 2000.00 || || 3000.00
+10/01/2012 || 1000.00 || || 1000.00
