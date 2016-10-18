163bf0fa9fd6c5de5ad029707bfa7d61ec42c419
Joseph <joseph.jr@gmail.com>
2016-10-18 10:33:03 +0100

    further encapsulation, remvoe att reader

--- bank.rb
+++ bank.rb
@@ -1,29 +1,41 @@
 class Bank
 
-def initialize
+
+def initialize(depo=" ")
   @balance = 0
-  @book = []
-end
-
-def deposit
-
-end
-
-def withdraw
-
+  @book = [[Time.new.strftime("%d/%m/%y"), depo," ", @balance]]
+  return @book
 end
 
 
-def print
-puts "date || credit || debit || balance"
+def deposit(credit)
+  @balance += credit
+  @book.push([Time.new.strftime("%d/%m/%y"), credit," ", @balance])
+  return @balance
+end
+
+def withdraw(debit)
+  @balance -= debit
+  @book.push([Time.new.strftime("%d/%m/%y"), " ", debit, @balance])
+  return @balance
+end
+
+
+def balance_book
+    puts "date || credit || debit || balance"
+    @book.each do |x|
+      puts x.join(" || ")
+    end
+end
+
+private
+attr_reader :balance
 
 end
 
-end
 
-
-
-
-14/01/2012 ||  500.00 || || 2500.00
-13/01/2012 || 2000.00 || || 3000.00
-10/01/2012 || 1000.00 || || 1000.00
+#
+#
+# 14/01/2012 ||  500.00 || || 2500.00
+# 13/01/2012 || 2000.00 || || 3000.00
+# 10/01/2012 || 1000.00 || || 1000.00
